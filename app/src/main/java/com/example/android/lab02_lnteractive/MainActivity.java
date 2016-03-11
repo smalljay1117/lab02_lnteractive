5e726a2a9932f06f4626462d9fb157a45a7f028e
smalljay1117 <smalljay1117>
2016-03-11 12:11:16 +0800

    number=2 NumberFormat

--- app/src/main/java/com/example/android/lab02_lnteractive/MainActivity.java
+++ app/src/main/java/com/example/android/lab02_lnteractive/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.lab02_lnteractive;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(2);
+
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(String.valueOf(number));
+
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        int price = 5;
+        int total = price * number;
+        String myString = NumberFormat.getCurrencyInstance().format(total);
+        priceTextView.setText(myString);
+
     }
 }
